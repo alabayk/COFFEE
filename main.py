512e92e39d017035b9524169e809dc39a41344d8
Imperor_Ivan <ivan.dremach@gmail.com>
2023-11-17 22:27:03 +0300

    adding files to github

---
+++ main.py
@@ -0,0 +1,31 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("main.ui", self)
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('coffee.sqlite')
+        db.open()
+        model = QSqlTableModel(self, db)
+        model.setTable('COFFEE')
+        model.select()
+
+        # Для отображения данных на виджете
+        # свяжем его и нашу модель данных
+        self.view.setModel(model)
+        self.setWindowTitle('COFFEE')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
